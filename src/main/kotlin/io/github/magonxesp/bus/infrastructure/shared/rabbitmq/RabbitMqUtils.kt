da76b8ed603f09b525ceab17d272e3de2f65ab13
MagonxESP <janma.360@gmail.com>
2024-04-09 17:05:14 +0200

    feat: create queue dead letters

--- src/main/kotlin/io/github/magonxesp/bus/infrastructure/shared/rabbitmq/RabbitMqUtils.kt
+++ src/main/kotlin/io/github/magonxesp/bus/infrastructure/shared/rabbitmq/RabbitMqUtils.kt
@@ -10,18 +10,45 @@ private fun String.prefixedQueueName(): String {
 	return this
 }
 
+/**
+ * Create the dead letter queue.
+ *
+ * @return The map with the arguments to send failed messages to this dead letter
+ */
+private fun Channel.createDirectDeadLetterQueue(
+	exchange: String,
+	routingKey: String,
+	queueName: String
+): Map<String, String> {
+	val prefixedQueueName = queueName.prefixedQueueName() + ".dlx"
+	val deadLetterExchange = "$exchange.dlx"
+	val deadLetterRoutingKey = "$routingKey.dlx"
+
+	exchangeDeclare(deadLetterExchange, "direct", true)
+	queueDeclare(prefixedQueueName, true, false, false, null)
+	queueBind(prefixedQueueName, deadLetterExchange, deadLetterRoutingKey)
+
+	return mapOf(
+		"x-dead-letter-exchange" to deadLetterExchange,
+		"x-dead-letter-routing-key" to deadLetterRoutingKey
+	)
+}
+
 fun Channel.createFanoutQueue(exchange: String, queueName: String) {
+	val routingKey = "$queueName.fanout".prefixedQueueName()
+	val deadLetterArgs = createDirectDeadLetterQueue(exchange, routingKey, queueName)
 	val prefixedQueueName = queueName.prefixedQueueName()
 
 	exchangeDeclare(exchange, "fanout", true)
-	queueDeclare(prefixedQueueName, true, false, false, null)
-	queueBind(prefixedQueueName, exchange, exchange + "_rk")
+	queueDeclare(prefixedQueueName, true, false, false, deadLetterArgs)
+	queueBind(prefixedQueueName, exchange, routingKey)
 }
 
 fun Channel.createDirectQueue(exchange: String, routingKey: String, queueName: String) {
+	val deadLetterArgs = createDirectDeadLetterQueue(exchange, routingKey, queueName)
 	val prefixedQueueName = queueName.prefixedQueueName()
 
 	exchangeDeclare(exchange, "direct", true)
-	queueDeclare(prefixedQueueName, true, false, false, null)
+	queueDeclare(prefixedQueueName, true, false, false, deadLetterArgs)
 	queueBind(prefixedQueueName, exchange, routingKey)
 }
