4094c9fb9b02699941f9ab76a0e14e7e9ab568c5
MagonxESP <janma.360@gmail.com>
2024-10-25 22:41:21 +0200

    feat: check if user and book exists

--- performance-test/new-user-create-offer.py
+++ performance-test/new-user-create-offer.py
@@ -1,11 +1,26 @@
-import time
 from locust import HttpUser, task
-
-import generators
+import time
 from common import default_headers
+import generators
 
 
 class MobileDeviceUser(HttpUser):
+
+    def ensure_user_exists(self, userId):
+        for _ in range(5):
+            with self.client.get(f"/users/{userId}", headers=default_headers, catch_response=True) as response:
+                response.success()
+                if response.status_code == 200:
+                    break
+            time.sleep(3)
+
+    def ensure_book_exists(self, bookId):
+        for _ in range(5):
+            with self.client.get(f"/books/{bookId}", headers=default_headers, catch_response=True) as response:
+                response.success()
+                if response.status_code == 200:
+                    break
+            time.sleep(3)
 
     @task
     def new_user_create_offer(self):
