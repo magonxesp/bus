a482d5539785fc01358ee73a7b645a8aabd193a1
MagonxESP <janma.360@gmail.com>
2024-10-29 14:01:39 +0100

    refactor: delete unused classes

--- bus/src/main/kotlin/io/github/magonxesp/bus/domain/shared/metadata/Metadata.kt
+++
@@ -1,7 +0,0 @@
-package io.github.magonxesp.bus.domain.shared.metadata
-
-import java.io.Serializable
-
-typealias MetadataValue = Serializable?
-typealias MutableMetadata = MutableMap<String, MetadataValue>
-typealias Metadata = Map<String, MetadataValue>
